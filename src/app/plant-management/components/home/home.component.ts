55327c42388213811c89b06c5a58b1974afe0a46
José Diego <JoseHuamani_20@hotmail.com>
2024-09-25 04:20:27 -0500

    feat(home): add home setion into app aplication

---
+++ src/app/plant-management/components/home/home.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatIcon} from "@angular/material/icon";
+import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
+import {MatProgressBar} from "@angular/material/progress-bar";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    MatGridList,
+    MatGridTile,
+    MatIcon,
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatProgressBar
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  taskItems : any[] = [];
+  gardenItems : any[] = [];
+  lifecycleitems : any[] = [];
+
+  constructor(private menuService: ServicePlantManagementService) {}
+  ngOnInit() {
+    this.menuService.getActivities().subscribe((data: any[]) => {
+      this.taskItems = data;
+    });
+    this.menuService.getlifecycle().subscribe((data: any[]) => {
+      this.lifecycleitems = data;
+    });
+    this.menuService.getgarden().subscribe((data: any[]) => {
+      this.gardenItems = data;
+    });
+  }
+}
